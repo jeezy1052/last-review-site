12b474e7d5eae8392672a5b3808ee7747e831a45
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-02-06 10:54:39 -0800

    before i add redux

---
+++ src/components/Card/PostComments/PostComments.js
@@ -0,0 +1,12 @@
+import React,{ useState, useRef } from 'react'
+import { TextField, Typography, Button } from '@mui/material'
+
+
+
+const PostComments = () => {
+  return (
+    <h1>Comment section</h1>
+  )
+}
+
+export default PostComments
